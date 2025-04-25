9ef490a4d44f747354c1a7831f906d91b2e250ef
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-04-25 19:35:04 +0200

    Fetchin data in page (json placeholder)

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <h1> The Wild Oasis. Welcome to Paradise.</h1>
+      <h1> The Wild Oasis. Welcome to Paradise!</h1>
       <Link href="/cabins">Explore luxory cabins</Link>
     </div>
   );
