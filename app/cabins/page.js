7cac429a94fb13840b304ede35f1b5bf593b01b9
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-04-30 20:11:15 +0200

    update

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -3,7 +3,7 @@ import Counter from "@/app/_components/Counter";
 export default function Page() {
   return (
     <div>
-      <h1>Cabins Page</h1>;
+      <h1>Cabins Page</h1>
     </div>
   );
 }
