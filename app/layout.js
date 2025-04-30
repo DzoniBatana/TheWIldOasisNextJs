e0c67990006ebecbec6b94368f5d34bde25e9627
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-04-30 20:40:28 +0200

    Update

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,18 @@ import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious Cabin hotel, located in heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-colors-primary-950 text-colors-primary-100 min-h-screen">
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
