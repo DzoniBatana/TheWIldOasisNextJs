d1ba0ef3638c3f42b76f0d83eda4a7a4d6c7d44c
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-05-15 20:04:32 +0200

    ContextApi for client component update

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -4,6 +4,7 @@ import { isWithinInterval } from "date-fns";
 import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { useReservation } from "./ReservationContext";
 
 function isAlreadyBooked(range, datesArr) {
   return (
@@ -16,6 +17,7 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ settings, cabin, bookedDates }) {
+  const { range, setRange, resetRange } = useReservation();
   // CHANGE
   const regularPrice = 23;
   const discount = 23;
@@ -72,7 +74,7 @@ function DateSelector({ settings, cabin, bookedDates }) {
         {range.from || range.to ? (
           <button
             className="border border-primary-800 py-2 px-4 text-sm font-semibold"
-            onClick={() => resetRange()}
+            onClick={resetRange}
           >
             Clear
           </button>
