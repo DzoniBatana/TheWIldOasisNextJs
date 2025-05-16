4023495efb9a99591f7c4aa196a969fafea8294e
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-05-16 22:06:11 +0200

    SignIn option, loginIn option

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  // ovo ovde je samo ako zelimo da nam ide samo ime npr"Natasa", a ne puno ime i prezime kao sto je sada, i onda dole samo ucitamo {firstName} umesto onoga sto sada ima
+  //const firstName = session.user.name.split(" ").at(0);
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Nikola
+      Welcome, {session.user.name}
     </h2>
   );
 }
