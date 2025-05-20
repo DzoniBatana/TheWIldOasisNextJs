cdd60555040f62414a1c47377b1a25f130fc4b8f
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-05-20 22:28:10 +0200

    Authentication process update

--- app/account/page.js
+++ app/account/page.js
@@ -6,6 +6,8 @@ export const metadata = {
 
 export default async function Page() {
   const session = await auth();
+  //console.log(session);
+
   // ovo ovde je samo ako zelimo da nam ide samo ime npr"Natasa", a ne puno ime i prezime kao sto je sada, i onda dole samo ucitamo {firstName} umesto onoga sto sada ima
   //const firstName = session.user.name.split(" ").at(0);
   return (
